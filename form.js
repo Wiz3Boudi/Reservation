62e44ccee51171a419fa76997c9cb9e1eeccb943
Your Name <your.email@example.com>
2025-08-20 21:19:58 +0300

    last changes

--- form.js
+++ form.js
@@ -1,3 +1,30 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
+      // create container 
 
+      const formContainer = document.createElement('div');
+      document.body.appendChild(formContainer)
+      formContainer.className = 'formContainer'
+
+      // create header 
+      const header = document.createElement('div')
+      header.className ='header';
+      formContainer.appendChild(header)
+
+      // craete h1
+      
+
+
+      // create form
+      const form = document.createElement('form')
+      formContainer.appendChild(form);
+
+      // create Label
+      function createLabel(){
+            const label = document.createElement('label');
+            form.appendChild(label)
+            
+
+      }
+
+      createLabel();
 })
